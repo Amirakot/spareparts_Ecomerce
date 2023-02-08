9c866ef394ec3756a7a3b1a744e01efe04ab223f
amirakot <amirakotb16270@yahoo.com>
2023-02-08 18:51:49 +0200

    second

--- Desktop/todolist2/src/app/list/list.component.ts
+++ Desktop/todolist2/src/app/list/list.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit,Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
-export class ListComponent implements OnInit {
-@Input() value=[];
-@Input() i=0;
+export class ListComponent implements OnInit, OnChanges {
+  @Input() value: string = '';
+  @Input() i = 0;
+  @Output() removeItemEvent = new EventEmitter();
+  // remove(value: string) {
+  //   // console.log(value);
 
-  constructor() {
+  // }
+  constructor() {}
 
-   }
-remove(i:any){
-console.log(this.i);
-  this.value.splice(i,1)
-}
-  ngOnInit(): void {
-  console.log(this.value);
+  ngOnChanges(changes: SimpleChanges): void {}
+  remove(ind: any) {
+    console.log(ind);
+     this.removeItemEvent.emit(ind);
+    // this.value.slice(i, i + 1);
+    // this.value = this.value.filter((v, i) => {
+    //   i != ind;
+    // });
   }
-
+  ngOnInit(): void {
+    console.log(this.value);
+  }
 }
--- Desktop/todolist2/src/app/output/output.component.ts
+++ Desktop/todolist2/src/app/output/output.component.ts
@@ -3,24 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
-  styleUrls: ['./output.component.css']
+  styleUrls: ['./output.component.css'],
 })
 export class OutputComponent implements OnInit {
-value:Array<any>;
-// i:number=0
-// i:number;
-add(e:any): void{
+  value: Array<any>;
+  // i:number=0
+  // i:number;
+  add(e: any): void {
     this.value.push(e);
-// console.log(e)
-console.log(this.value);
-}
-  constructor() {
-this.value=[];
-
-   }
-
-  ngOnInit(): void {
-
+    // console.log(e)
+    console.log(this.value);
   }
+  remove(e: any): void {
+    console.log(e, 'reeeeeeeeeeemove');
+    this.value.splice(e, 1);
+    console.log(this.value);
+  }
+  constructor() {
+    this.value = [];
+  }
+  //  function remove(){
 
+  //  }
+
+  ngOnInit(): void {}
 }
